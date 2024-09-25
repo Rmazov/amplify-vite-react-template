58912304be556072337c165d94354e26c0d6e578
Ricardo Mazo Velasquez <97861713+Rmazov@users.noreply.github.com>
2024-09-25 17:50:56 -0500

    added authenticator

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
 
+import { Authenticator } from '@aws-amplify/ui-react'
+import '@aws-amplify/ui-react/styles.css'
+
 const client = generateClient<Schema>();
 
 function App() {
@@ -21,6 +24,9 @@ function App() {
   }
 
   return (
+        
+    <Authenticator>
+      {({ signOut }) => (
     <main>
       <h1>My todos</h1>
       <button onClick={createTodo}>+ new</button>
@@ -40,6 +46,9 @@ function App() {
         </a>
       </div>
     </main>
+        
+      )}
+      </Authenticator>
   );
 }
 
